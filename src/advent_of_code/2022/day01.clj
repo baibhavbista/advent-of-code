421b5cfb7d4bff7aeb233bf654a7eaf9ba842f14
Baibhav Bista <baibhavbista@gmail.com>
2022-12-01 22:42:10 +0545

    fix embarrasing usage of reduce where I could've used an apply

--- src/advent_of_code/2022/day01.clj
+++ src/advent_of_code/2022/day01.clj
@@ -13,19 +13,19 @@
        ;; we now have (("4887" "9307") ("8895" "8136" "6292" "10177" "4077" "8228") ...)
        (map (fn [list-cal-strs]
               (->> (map #(Integer/parseInt %) list-cal-strs)
-                   (reduce +))))))
+                   (apply +))))))
 
 (def part-1-answer
   "Maximum number of calories carried by any elf"
   (->> total-calories-carried-by-each-elf
-       (reduce max)))
+       (apply max)))
 
 (def part-2-answer
   "Sum of calories carried by the 3 elves carrying the 3 highest number of calories"
   (->> total-calories-carried-by-each-elf
        sort
        (take-last 3)
-       (reduce +)))
+       (apply +)))
 
 (defn -main []
   (println "part-1 answer:" part-1-answer)
