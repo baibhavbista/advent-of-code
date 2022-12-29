bb62c553a1aac9d26d65d581ef52af8f9a5b6c7e
Baibhav Bista <baibhavbista@gmail.com>
2022-12-29 16:23:57 +0545

    2022 day 16 part 2

--- src/advent_of_code/2022/day16.clj
+++ src/advent_of_code/2022/day16.clj
@@ -4,7 +4,7 @@
    [clojure.string :as str]
    [clojure.set]))
 
-(def input-filename "inputs/2022/day16.txt")
+(def input-filename "inputs/2022/day16.txt" #_"inputs/2022/day16-sample.txt")
 
 (defn valve-spec-str->valve-spec [valve-spec-str]
   (let [[_ valve-name flow-rate-str connected-valves-str]
@@ -20,7 +20,11 @@
                       (map valve-spec-str->valve-spec)
                       (into {})))
 
-(def TOTAL-MINUTES 30)
+(def non-zero-flow-valves-set
+  (->> valve-specs
+       (remove (comp zero? :flow-rate second))
+       (map first)
+       (into #{})))
 
 (defn move-to-valve-step [state new-valve]
   (assoc state :at-valve new-valve))
@@ -35,7 +39,7 @@
 (defn get-next-states [state minutes-left]
   (let [{:keys [at-valve opened-valves-set]} state
         connected-valves (get-in valve-specs [at-valve :connected-valves])]
-    (if (= opened-valves-set (keys valve-specs))
+    (if (= opened-valves-set non-zero-flow-valves-set)
       ;; no further movement necessary
       (list state)
       (cond-> (map (partial move-to-valve-step state) connected-valves)
@@ -53,8 +57,9 @@
                                       (apply max))]
                 (assoc (first maybe-multiple-states-with-maybe-diff-pressure) :pressure-if-stop-exploration max-pressure))))))
 
-(time (let [all-states-after-30-mins
-            (loop [minutes-left TOTAL-MINUTES
+(time (let [total-minutes 30
+            all-states-after-30-mins
+            (loop [minutes-left total-minutes
                    frontier     '({:at-valve                     "AA"
                                    :pressure-if-stop-exploration 0
                                    :opened-valves-set            #{}})]
@@ -79,4 +84,39 @@
 ;;  so that would be (/ (* (Math/pow 2 15) 1500) 1000 60) = 819 minutes lollll
 
 ;; giving up on this for today
-;; FIXME: Come back later and solve part 2 for this problem. Might need to look into graph algorithms (doesn't look like BFS/DFS/A* is going to cut it for this one)
+
+;; Continued on Dec 29, 2022 because I want all 50 stars!
+
+;; after some looking into the issue, realize that we can reuse from the 26 minute loop
+;; get all states and from them get mapping of opened-valves-set to max possible pressure if stop exploration
+;; then for all pairs in it which do not intersect
+
+(time (let [total-mins 26
+            all-states-including-intermediate
+            (loop [minutes-left total-mins
+                   frontier     '({:at-valve                     "AA"
+                                   :pressure-if-stop-exploration 0
+                                   :opened-valves-set            #{}})
+                   earlier-states '()]
+              #_(println "loop: minutes-left (count frontier): " minutes-left (count frontier))
+              (if (zero? minutes-left)
+                (into earlier-states frontier)
+                (recur (dec minutes-left)
+                       (->> frontier
+                            (mapcat #(get-next-states % minutes-left))
+                            compress-frontier-by-merging-states-different-only-in-pressure)
+                       (into earlier-states frontier))))
+            opened-valves->max-pressure
+            (reduce (fn [acc-map {:as _state :keys [opened-valves-set pressure-if-stop-exploration]}]
+                      (let [max-pressure (max pressure-if-stop-exploration
+                                              (get acc-map opened-valves-set 0))]
+                        (assoc acc-map opened-valves-set max-pressure)))
+                    {}
+                    all-states-including-intermediate)]
+        (->> (for [[person-opened-set   person-pressure]   opened-valves->max-pressure
+                   [elephant-opened-set elephant-pressure] opened-valves->max-pressure
+                   :when (not-any? person-opened-set elephant-opened-set)]
+               (+ person-pressure elephant-pressure))
+             (apply max))))
+
+;; answer 2: 2261
